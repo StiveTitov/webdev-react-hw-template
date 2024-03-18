90550df7bb04839418bd4be0541341bb69dbcea3
StiveTitov <123_dem_321@mail.ru>
2024-03-18 23:22:56 +0300

    add

--- skypro-music/src/components/FilterWrapper/FilterWrapper.tsx
+++ skypro-music/src/components/FilterWrapper/FilterWrapper.tsx
@@ -14,9 +14,9 @@ export default function FilterWrapper() {
         <>
             <div className={styles.centerblock__filter}>
                 <div className={styles.filter__title}>Искать по:</div>
-                <Filter title="исполнителю" list={authors} isOpen={activeFilter === "author"} onClick={() => handleFiltreClick("author")} />
-                <Filter title="году выпуска" list={years} isOpen={activeFilter === "year"} onClick={() => handleFiltreClick("year")} />
-                <Filter title="жанру" list={genres} isOpen={activeFilter === "genre"} onClick={() => handleFiltreClick("genre")} />
+                <Filter top={290} left={23} title="исполнителю" list={authors} isOpen={activeFilter === "author"} onClick={() => handleFiltreClick("author")} />
+                <Filter top={290} left={31} title="году выпуска" list={years} isOpen={activeFilter === "year"} onClick={() => handleFiltreClick("year")} />
+                <Filter top={290} left={39} title="жанру" list={genres} isOpen={activeFilter === "genre"} onClick={() => handleFiltreClick("genre")} />
 
             </div>
         </>
