f07d69566edd4b266324fe564ca211d438e41266
StiveTitov <123_dem_321@mail.ru>
2024-04-07 22:36:15 +0300

    add

--- skypro-music/jest.config.ts
+++ skypro-music/jest.config.ts
@@ -95,7 +95,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -201,4 +203,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
