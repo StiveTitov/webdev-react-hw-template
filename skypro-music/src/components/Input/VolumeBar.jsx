f501f3c8ea8b4e8167fc3655dee12f654598a0d3
StiveTitov <123_dem_321@mail.ru>
2024-04-10 23:19:57 +0300

    add

--- skypro-music/src/components/Input/VolumeBar.jsx
+++ skypro-music/src/components/Input/VolumeBar.jsx
@@ -1,19 +1,16 @@
-'use client'
+"use client";
 
 import styles from "./VolumeBar.module.css";
 
-export default function VolumeBar({ volume, handleVolume}) {
-  
-
+export default function VolumeBar({ volume, handleVolume }) {
   return (
-    <input className={styles.barVolumeProgress}
+    <input
+      className={styles.barVolumeProgress}
       type="range"
       min={0}
       max={100}
       value={volume}
-      
       onChange={handleVolume}
-      
     />
   );
 }
